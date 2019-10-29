ee03c33dc4d24128395e5b413e8691915c876f41
sebastyan <sebastian.mota.gomez@gmail.com>
2019-10-29 09:34:11 +0100

    In case of the retry document does not exist on database a DeleteRetryException will be throw in order to inform about that. This could be a signal that database could be not working propely.

---
+++ PersistentRetryTemplate/Retry/DeleteRetryException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PersistentRetryTemplate.Retry
+{
+    public class DeleteRetryException : Exception
+    {
+        public DeleteRetryException(string message) : base(message) { }
+
+        public DeleteRetryException(string message, Exception cause) : base(message, cause) { }
+    }
+}
